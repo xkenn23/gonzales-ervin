a7600a7496365320a01dd439a19e17e8cfc11b4e
xkenn <ervin.gonzales.inno@gmail.com>
2024-11-28 22:47:25 +0800

    add day 17 lesson and activity

---
+++ D17/activity/activity-day17/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import AppNavbar from "./components/AppNavBar";
+
+function App() {
+  return (
+    <div>
+      <AppNavbar />
+    </div>
+  );
+}
+
+export default App;
